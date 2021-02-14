bafe6c6360bd400df73a95036a3f5507bd6717c0
Courtney Fox <courtney.fox@me.com>
2021-02-14 17:48:35 -0500

    added questions to the questions array

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,40 @@ const inquirer = ('inquirer');
 const utils = ('utils');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: '# title of project'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: '## Description'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: '## Installation Instructions'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: '## Usage'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: '## Contributing'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: '## Tests'
+    }
+];
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

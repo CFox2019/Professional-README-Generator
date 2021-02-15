574db9da6f6f2a9b1db0b401a64c23e903d67112
Courtney Fox <courtney.fox@me.com>
2021-02-15 10:39:14 -0500

    updated questions and the generateREADME function

---
+++ Develop/README.md
@@ -0,0 +1,12 @@
+
+    # Professional README Generator,
+    ## Description
+    The Professional README Generator allows the user to quickly write a README about their projects so that they can spend more time on working on their actual project.,
+    ## installation
+    Here are the installation instructions,
+    ## usage
+    The project will be used for quickly making a README,
+    ## contributions
+    Markdown site,
+    ## tests
+    Here are the tests I ran.
--- Develop/index.js
+++ Develop/index.js
@@ -1,49 +1,82 @@
 // TODO: Include packages needed for this application
-const fs = ('fs');
-const inquirer = ('inquirer');
-const utils = ('utils');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'title',
-        message: '# title of project'
+        message: 'What is the title of the project?'
     },
     {
         type: 'input',
         name: 'description',
-        message: '## Description'
+        message: 'Description of the project'
     },
     {
         type: 'input',
         name: 'installation',
-        message: '## Installation Instructions'
+        message: 'What are the installation instructions?'
     },
     {
         type: 'input',
         name: 'usage',
-        message: '## Usage'
+        message: 'How will the project be used? (Include screenshots)'
     },
     {
         type: 'input',
         name: 'contribution',
-        message: '## Contributing'
+        message: 'List any contributers or any third-party assets that require attribution.'
     },
     {
         type: 'input',
         name: 'tests',
-        message: '## Tests'
+        message: 'If you ran any tests, include examples.'
     }
 ];
 
+function promptUser() {
+    return inquirer.prompt(questions)
+};
 
+function generateReadMe(answers) {
+    return `
+    # ${answers.title},
+    ## Description
+    ${answers.description},
+    ## installation
+    ${answers.installation},
+    ## usage
+    ${answers.usage},
+    ## contributions
+    ${answers.contribution},
+    ## tests
+    ${answers.tests}
+`;
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+promptUser()
+.then(function(answers) {
+    const md = generateReadMe(answers);
 
-// TODO: Create a function to initialize app
-function init() {}
+    return writeFileAsync('README.md', md);
+})
+.then(function() {
+    console.log('Successfully wrote to README.md!')
+})
+.catch(function(err) {
+    console.error(err);
+})
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();

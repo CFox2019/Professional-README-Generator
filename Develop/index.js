19fed5ab0f1cb5abb14f21727ea3cb62cd63f8ed
Courtney Fox <courtney.fox@me.com>
2021-02-17 10:36:09 -0500

    added checkbox type with licensing options to index

--- Develop/index.js
+++ Develop/index.js
@@ -36,6 +36,12 @@ const questions = [
         type: 'input',
         name: 'tests',
         message: 'If you ran any tests, include examples.'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'What license do you want to use?',
+        choices: ['None', 'Apache License 2.0', 'GNU Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
     }
 ];
 
@@ -45,17 +51,25 @@ function promptUser() {
 
 function generateReadMe(answers) {
     return `
-    # ${answers.title},
-    ## Description
-    ${answers.description},
-    ## installation
-    ${answers.installation},
-    ## usage
-    ${answers.usage},
-    ## contributions
-    ${answers.contribution},
-    ## tests
-    ${answers.tests}
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## installation
+${answers.installation}
+
+## usage
+${answers.usage}
+
+## contributions
+${answers.contribution}
+
+## tests
+${answers.tests}
+
+## License
+${answers.license}
 `;
 }
 

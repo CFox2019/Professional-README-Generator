b487f3792cc9461f22e309f15421dd5c7ddaab3a
Courtney Fox <courtney.fox@me.com>
2021-02-17 23:00:26 -0500

    added license badges, table of contents

--- Develop/README.md
+++ Develop/README.md
@@ -1,20 +1,38 @@
 
-# asdfs
+![](https://img.shields.io/badge/license--brightgreen)
+
+# asdfasdf
+
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
 
 ## Description
-sdfasfd
+asdasdf
 
-## installation
-sdfasdf
+## Installation
+asdfasd
 
-## usage
-asfsd
+## Usage
+asdfasdf
 
-## contributions
-sdfasd
+## Contributing
+asdfsdf
 
-## tests
-asdfsf
+## Tests
+asdfds
+
+## Questions
+GitHub: [asdfsdf](asdfsdf)
+
+To contact me with additional questions, please reach me at:
+[asdfsdf](asdfsdf)
 
 ## License
-
+This project is covered under the  license.
--- Develop/index.js
+++ Develop/index.js
@@ -25,7 +25,7 @@ const questions = [
     {
         type: 'input',
         name: 'usage',
-        message: 'How will the project be used? (Include screenshots)'
+        message: 'How do you use the application? (Include screenshots)'
     },
     {
         type: 'input',
@@ -38,10 +38,26 @@ const questions = [
         message: 'If you ran any tests, include examples.'
     },
     {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'githublink',
+        message: 'What is your GitHub profile link?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
         type: 'checkbox',
         name: 'license',
         message: 'What license do you want to use?',
-        choices: ['None', 'Apache License 2.0', 'GNU Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
+        choices: [
+            'None', 'Apache 2.0', 'GNU Public v3.0',  'MIT', 'Boost Software License 1.0',  'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public v3.0', 'GNU General Public v2.0', 'GNU Lesser General Public v2.1', 'Mozilla Public 2.0', 'The Unlicense']
     }
 ];
 
@@ -51,25 +67,43 @@ function promptUser() {
 
 function generateReadMe(answers) {
     return `
+![${answers.license}](https://img.shields.io/badge/license-${escape(answers.license)}-brightgreen)
+
 # ${answers.title}
+
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
 
 ## Description
 ${answers.description}
 
-## installation
+## Installation
 ${answers.installation}
 
-## usage
+## Usage
 ${answers.usage}
 
-## contributions
+## Contributing
 ${answers.contribution}
 
-## tests
+## Tests
 ${answers.tests}
 
+## Questions
+GitHub: [${answers.github}](${answers.githublink})
+
+To contact me with additional questions, please reach me at:
+[${answers.email}](${answers.email})
+
 ## License
-${answers.license}
+This project is covered under the ${answers.license} license.
 `;
 }
 
